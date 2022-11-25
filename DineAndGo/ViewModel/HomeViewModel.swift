e8c68d7728a40127d7228dd6d2b46fcd7f0c6ad7
Chengtao <chengtaolinctl@gmail.com>
2022-11-25 17:02:05 -0500

    update part1 4:32

---
+++ DineAndGo/ViewModel/HomeViewModel.swift
@@ -0,0 +1,24 @@
+//
+//  HomeViewModel.swift
+//  DineAndGo
+//
+//  Created by CHENGTAO on 11/25/22.
+//
+
+import SwiftUI
+
+class HomeViewModel: ObservableObject{
+    @Published var search = ""
+}
+
+//struct HomeViewModel: View {
+//    var body: some View {
+//        Text("Hello, World!")
+//    }
+//}
+//
+//struct HomeViewModel_Previews: PreviewProvider {
+//    static var previews: some View {
+//        HomeViewModel()
+//    }
+//}
